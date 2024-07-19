f7b6bb48089187b2f87df0a2453fa52abcf214dd
HirotaKousei <r.m10.0405kousei@gmail.com>
2024-07-19 13:19:33 +0900

    追加

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="card text-bg-dark">
-    <img src="{{ asset('/storage//30109534_m.jpg')}}" class="card-img opacity-25 .img-fluid" alt="画像">
+    <img src="{{ asset('/storage/30109534_m.jpg')}}" class="card-img opacity-25 .img-fluid" alt="画像">
     <div class="card-img-overlay">
         <div class="row justify-content-center">
             <div class="col-md-8">
